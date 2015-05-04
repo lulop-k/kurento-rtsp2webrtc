e1299ef45489801ae078ce52be2d82526e0e472e
Luis López <lulop@kurento.com>
2015-05-04 22:53:55 +0200

    Adapated to KMS v6.0 trickle ice

--- js/index.js
+++ js/index.js
@@ -39,82 +39,129 @@ if (args.ice_servers) {
   console.log("Use freeice")
 }
 
-var videoInput;
-var address;
-var webRtcPeer;
-var pipeline;
 
-window.onload = function() {
-	console = new Console('console', console);
-	videoInput = document.getElementById('videoInput');
-	address = document.getElementById('address');
+window.addEventListener('load', function(){
+  console = new Console('console', console);
+	var videoOutput = document.getElementById('videoOutput');
+	var address = document.getElementById('address');
 	address.value = 'http://files.kurento.org/video/puerta-del-sol.ts';
-}
+  var pipeline;
+  var webRtcPeer;
 
-function start() {
-	if(!address.value){
-	  window.alert("You must set the video source URL first");
-	  return;
-	}
-	address.disabled = true;
-	showSpinner(videoOutput);
-	webRtcPeer = kurentoUtils.WebRtcPeer.startRecvOnly(videoOutput, onOffer, onError);
-}
+  startButton = document.getElementById('start');
+  startButton.addEventListener('click', start);
 
-function stop() {
-	address.disabled = false;
-	if (webRtcPeer) {
-		webRtcPeer.dispose();
-		webRtcPeer = null;
-	}
-	if(pipeline){
-		pipeline.release();
-		pipeline = null;
-	}
-	hideSpinner(videoOutput);
-}
+  stopButton = document.getElementById('stop');
+  stopButton.addEventListener('click', stop);
 
-function onOffer(sdpOffer){
-	kurentoClient(args.ws_uri, function(error, kurentoClient) {
-		if(error) return onError(error);
+  function start() {
+  	if(!address.value){
+  	  window.alert("You must set the video source URL first");
+  	  return;
+  	}
+  	address.disabled = true;
+  	showSpinner(videoOutput);
+    var options = {
+      remoteVideo : videoOutput
+    };
+    webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options,
+      function(error){
+        if(error){
+          return console.error(error);
+        }
+        webRtcPeer.generateOffer(onOffer);
+        webRtcPeer.peerConnection.addEventListener('iceconnectionstatechange', function(event){
+          if(webRtcPeer && webRtcPeer.peerConnection){
+            console.log("oniceconnectionstatechange -> " + webRtcPeer.peerConnection.iceConnectionState);
+            console.log('icegatheringstate -> ' + webRtcPeer.peerConnection.iceGatheringState);
+          }
+        });
+    });
+  }
 
-		kurentoClient.create("MediaPipeline", function(error, p) {
-			if(error) return onError(error);
+  function onOffer(error, sdpOffer){
+    if(error) return onError(error);
 
-			pipeline = p;
+  	kurentoClient(args.ws_uri, function(error, kurentoClient) {
+  		if(error) return onError(error);
 
-			pipeline.create("PlayerEndpoint", {uri: address.value}, function(error, player){
-			  if(error) return onError(error);
-			  
-			  pipeline.create("WebRtcEndpoint", function(error, webRtc){
-				if(error) return onError(error);
+  		kurentoClient.create("MediaPipeline", function(error, p) {
+  			if(error) return onError(error);
 
-				webRtc.processOffer(sdpOffer, function(error, sdpAnswer){
-					if(error) return onError(error);
+  			pipeline = p;
 
-					webRtcPeer.processSdpAnswer(sdpAnswer);
-				});
+  			pipeline.create("PlayerEndpoint", {uri: address.value}, function(error, player){
+  			  if(error) return onError(error);
 
-				player.connect(webRtc, function(error){
-					if(error) return onError(error);
+  			  pipeline.create("WebRtcEndpoint", function(error, webRtcEndpoint){
+  				if(error) return onError(error);
 
-					console.log("PlayerEndpoint-->WebRtcEndpoint connection established");
-					
-					player.play(function(error){
-					  if(error) return onError(error);
+          setIceCandidateCallbacks(webRtcEndpoint, webRtcPeer, onError);
 
-					  console.log("Player playing ...");
-					});
-				});
-			});
-			});
-		});
-	});
+  				webRtcEndpoint.processOffer(sdpOffer, function(error, sdpAnswer){
+  					if(error) return onError(error);
+
+            webRtcEndpoint.gatherCandidates(onError);
+
+  					webRtcPeer.processAnswer(sdpAnswer);
+  				});
+
+  				player.connect(webRtcEndpoint, function(error){
+  					if(error) return onError(error);
+
+  					console.log("PlayerEndpoint-->WebRtcEndpoint connection established");
+
+  					player.play(function(error){
+  					  if(error) return onError(error);
+
+  					  console.log("Player playing ...");
+  					});
+  				});
+  			});
+  			});
+  		});
+  	});
+  }
+
+  function stop() {
+    address.disabled = false;
+    if (webRtcPeer) {
+      webRtcPeer.dispose();
+      webRtcPeer = null;
+    }
+    if(pipeline){
+      pipeline.release();
+      pipeline = null;
+    }
+    hideSpinner(videoOutput);
+  }
+
+});
+
+function setIceCandidateCallbacks(webRtcEndpoint, webRtcPeer, onError){
+  webRtcPeer.on('icecandidate', function(candidate){
+    console.log("Local icecandidate " + JSON.stringify(candidate));
+
+    candidate = kurentoClient.register.complexTypes.IceCandidate(candidate);
+
+    webRtcEndpoint.addIceCandidate(candidate, onError);
+
+  });
+  webRtcEndpoint.on('OnIceCandidate', function(event){
+    var candidate = event.candidate;
+
+    console.log("Remote icecandidate " + JSON.stringify(candidate));
+
+    webRtcPeer.addIceCandidate(candidate, onError);
+  });
 }
 
 function onError(error) {
-	if(error) console.error(error);
-	stop();
+  if(error)
+  {
+    console.error(error);
+    stop();
+  }
 }
 
 function showSpinner() {
